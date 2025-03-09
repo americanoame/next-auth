8519dd6e4a7c344c3e3772de2cc8ea414852c266
Andre Silva <andrenations@gmail.com>
2025-03-09 17:28:33 -0400

    Add Home component with welcome message and styled heading

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center mt-42">
+      <h1 className="text-3xl text-blue-500 font-bold underline">
+        Welcome to Next Auth
+      </h1>
+    </div>
+  );
+}
