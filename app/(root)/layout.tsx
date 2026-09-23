870d6d4cd2eba3ab17f1d41fc19c571c9ccce32b
Andre Silva <andrenations@gmail.com>
2026-09-23 16:44:32 -0400

    add layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,7 @@
+import { SessionProvider } from "next-auth/react";
 import Header from "@/components/shared/header";
 import Footer from "@/components/footer";
+
 
 export default function RootLayout({
   children,
@@ -7,10 +9,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex h-screen flex-col">
-      <Header />
-      <main className="flex-1 wrapper">{children}</main>
-      <Footer />
-    </div>
+    <SessionProvider>
+      <div className="flex h-screen flex-col">
+        <Header />
+        <main className="flex-1 wrapper">{children}
+          
+        </main>
+        <Footer />
+      </div>
+    </SessionProvider>
   );
 }
