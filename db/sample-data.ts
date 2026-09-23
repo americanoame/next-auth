a8c57739211b02a89fb86620990dbfcb435815eb
Andre Silva <andrenations@gmail.com>
2026-09-23 16:57:08 -0400

    Add sample seed data for admin and user roles

---
+++ db/sample-data.ts
@@ -0,0 +1,23 @@
+import { hashSync } from 'bcrypt-ts-edge';
+
+const sampleData = {
+
+  users: [
+    {
+      name: 'John',
+      email: 'admin@example.com',
+      password: hashSync('123456', 10),
+      role: 'admin',
+    },
+    {
+      name: 'Jane',
+      email: 'jane@example.com',
+      password: hashSync('123456', 10),
+      role: 'user',
+    },
+  ],
+
+ 
+};
+
+export default sampleData;
