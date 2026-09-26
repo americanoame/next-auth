f1be57b779079d479628322540b11e9fd52b9f61
Andre Silva <andrenations@gmail.com>
2026-09-25 23:28:30 -0400

    get rid if the prisma adapter

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,20 +1,17 @@
-import { Pool, neonConfig } from '@neondatabase/serverless';
-import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client';
-import ws from 'ws';
-
-neonConfig.webSocketConstructor = ws;
 
 const globalForPrisma = globalThis as unknown as {
   prisma: ReturnType<typeof createPrismaClient> | undefined;
 };
 
 function createPrismaClient() {
-  const connectionString = `${process.env.DATABASE_URL}`;
-  const pool = new Pool({ connectionString });
-  const adapter = new PrismaNeon(pool as any);
+  const connectionString = process.env.DATABASE_URL;
 
-  return new PrismaClient({ adapter });
+  if (!connectionString) {
+    throw new Error('DATABASE_URL environment variable is missing. Check your .env settings.');
+  }
+
+  return new PrismaClient();
 }
 
 export const prisma = globalForPrisma.prisma ?? createPrismaClient();
