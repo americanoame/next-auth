7803db3a2c7e5cd7730720e23809d3856c6eb477
Andre Silva <andrenations@gmail.com>
2026-09-25 23:25:29 -0400

    add loader

---
+++ app/loading.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+import loader from '@/assets/loader.gif';
+
+const Loading = () => {
+
+
+
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        width: '100vw',
+      }}
+    >
+      <Image src={loader} width={150} height={150} alt='Loading...' 
+      style={{ width: 'auto', height: 'auto' }}
+      />
+    </div>
+  );
+};
+
+export default Loading;
